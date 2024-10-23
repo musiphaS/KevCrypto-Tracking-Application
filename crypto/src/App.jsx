eb685f0ef58f2b85bc45738f367f47a077b50fe1
Kelvin <kelvinmusipha@gmail.com>
2024-10-23 15:53:48 +0200

    installed packages

--- crypto/src/App.jsx
+++ crypto/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/navbar/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Coin from './pages/Coin/Coin'
-import Footer from './components/footer/Footer'
+import Footer from './components/footer/Footer';
 
 
 const App = () => {
