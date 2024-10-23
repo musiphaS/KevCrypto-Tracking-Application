42e02fbcb70e1fea1b0322adf42cb9fd50fd43af
Kelvin <kelvinmusipha@gmail.com>
2024-10-23 16:00:29 +0200

    vite

--- crypto/src/main.jsx
+++ crypto/src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import CoinContextProvider from './context/CoinContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <CoinContextProvider>
           <App />        
       </CoinContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
