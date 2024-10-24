8411cab99bc08e1df327aa049b20743583e72430
Kelvin <kelvinmusipha@gmail.com>
2024-10-24 11:18:44 +0200

    fontawesome

--- crypto/src/components/themeSwitcher/ThemeSwitcher.jsx
+++ crypto/src/components/themeSwitcher/ThemeSwitcher.jsx
@@ -1,30 +1,34 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // we use it for icons
-import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'; // icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useState } from 'react';
 
-
 const ThemeSwitcher = () => {
-    const [isDarkMode, setIsDarkMode] = useState(false); //track dark mode state 
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
-    // update background when isDarkMode
     useEffect(() => {
         document.documentElement.setAttribute(
-          'data-theme', 
-          isDarkMode ? 'dark' : 'light');
+          'data-theme',
+          isDarkMode ? 'dark' : 'light'
+        );
     }, [isDarkMode]);
 
-    const handleThemeSwitcher = () => setIsDarkMode(!isDarkMode); //toggle effect
+    const handleThemeSwitcher = () => setIsDarkMode(!isDarkMode);
 
-  return (
-    <div className='theme-wrap'>
-        <FontAwesomeIcon icon={faSun} color={isDarkMode ? '#fff' : '#222325'} />
-        <label className='theme-switcher' htmlFor='checkbox'>
-        <input type='checkbox' id='checkbox' checked={isDarkMode} onChange={handleThemeSwitcher} />
-        <div className='slider-rounded'></div>
-        </label>
-        <FontAwesomeIcon icon={faMoon} color={isDarkMode ? '#fff' : '#222325'} />
-    </div>
-  )
-}
+    return (
+        <div className='theme-wrap'>
+            <FontAwesomeIcon icon={faSun} color={isDarkMode ? '#fff' : '#222325'} />
+            <label className='theme-switcher' htmlFor='checkbox'>
+                <input 
+                    type='checkbox' 
+                    id='checkbox' 
+                    checked={isDarkMode} 
+                    onChange={handleThemeSwitcher} 
+                />
+                <div className='slider-rounded'></div>
+            </label>
+            <FontAwesomeIcon icon={faMoon} color={isDarkMode ? '#fff' : '#222325'} />
+        </div>
+    );
+};
 
-export default ThemeSwitcher
+export default ThemeSwitcher;
