43848bc360e8543a84f5df564b69b04eb8299740
Kelvin <kelvinmusipha@gmail.com>
2024-10-24 11:12:36 +0200

    fonts

--- crypto/vite.config.js
+++ crypto/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['axios']
+      external: ['axios', '@fortawesome/free-solid-svg-icons']
     }
   }
 })
